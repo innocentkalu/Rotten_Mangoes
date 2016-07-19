e73d5b04b7f3f41b85cd3d0e16a632c75fcb3f3d
innocentkalu <ikalu.ik@gmail.com>
2016-07-19 19:46:50 +0000

    Created Flash messages and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   resources :movies
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
