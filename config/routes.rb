c8e8bb6cdb3e24f8b0ad8a42748d12dddc42547f
innocentkalu <ikalu.ik@gmail.com>
2016-07-19 20:35:04 +0000

    Added the reviews logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
 end
+
+
