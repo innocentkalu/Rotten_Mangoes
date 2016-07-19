0b7285b4404b23763e0bd717ace540df1671de16
innocentkalu <ikalu.ik@gmail.com>
2016-07-19 18:00:25 +0000

    Created the new,edit,show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'movies/index'
-
-  get 'movies/show'
-
-  get 'movies/new'
-
-  get 'movies/edit'
-
-  get 'movies', to: 'movies#index'
+  resources :movies
 
 end
